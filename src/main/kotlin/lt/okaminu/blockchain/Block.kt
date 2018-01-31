22148ae268c1593f619d0bf72c30ee922ea17545
Okaminu <aurimasdgt@gmail.com>
2018-01-31 16:23:09 +0200

    hash added

--- src/main/kotlin/lt/okaminu/blockchain/Block.kt
+++ src/main/kotlin/lt/okaminu/blockchain/Block.kt
@@ -1,3 +1,10 @@
 package lt.okaminu.blockchain
 
-data class Block(val data: String, val timestamp: Long)
+import java.time.Instant
+
+data class Block(
+        val data: String,
+        val timestamp: Long = Instant.now().epochSecond,
+        val hash: String = "",
+        val previousHash: String = ""
+)
--- src/main/kotlin/lt/okaminu/blockchain/Blockchain.kt
+++ src/main/kotlin/lt/okaminu/blockchain/Blockchain.kt
@@ -1,16 +1,16 @@
 package lt.okaminu.blockchain
 
-import java.time.Instant
-
 class Blockchain(private val blocks: ArrayList<Block> = ArrayList()) {
 
     fun addBlock(data: String) {
-        blocks.add(Block(data, Instant.now().epochSecond))
+        blocks.add(Block(data))
     }
 
     fun printAllBlocks() = blocks.forEach {
         println("Data: ${it.data}")
         println("Timestamp: ${it.timestamp}")
+        println("Hash: ${it.hash}")
+        println("Previous hash: ${it.previousHash}")
         println()
     }
 }
