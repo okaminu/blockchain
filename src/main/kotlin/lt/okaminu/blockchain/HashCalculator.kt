f4d105c423adf227012539b3ed100c9fe41e7836
Okaminu <aurimasdgt@gmail.com>
2018-01-31 17:48:55 +0200

    calculates hash

--- src/main/kotlin/lt/okaminu/blockchain/HashCalculator.kt
+++ src/main/kotlin/lt/okaminu/blockchain/HashCalculator.kt
@@ -1,10 +1,15 @@
 package lt.okaminu.blockchain
 
+import java.security.MessageDigest
+
 fun calculate(block: Block) : Pair<Int, String>{
 
     val blockAsString = "${block.data}${block.previousHash}${block.timestamp}${block.nounce}"
     var nounce = 1
 
+    val md = MessageDigest.getInstance("SHA-256")
+    val digest = md.digest(blockAsString.toByteArray())
+    val hash = digest.fold("", { str, byte -> str + "%x02".format(byte) })
 
-    return Pair(nounce, blockAsString)
+    return Pair(nounce, hash)
 }
